2df56353c61e29a81258ea5a9ef2846ec934a76c
ProbstenHias <matthias.weilinger@gmx.de>
2022-11-16 15:53:30 +0100

    Add print script

---
+++ print-zarquon.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "usage: ./print-uni uniuser filename"
+    exit 1
+fi
+
+UNIUSER="$1"
+
+TARGET=$UNIUSER@marvin.informatik.uni-stuttgart.de
+
+if [ -z "$2" ]; then
+    echo "usage: ./print-zarquon uniuser filename"
+    exit 1
+fi
+
+FILE="$2"
+
+PRINTER=${3:-zarquon}
+
+FILENAME=$(basename $FILE)
+
+echo "PRINTING: $FILE @ $PRINTER at $TARGET"
+
+scp $FILE $TARGET:~/Downloads
+ssh $TARGET "lp -d $PRINTER ~/Downloads/$FILENAME"
